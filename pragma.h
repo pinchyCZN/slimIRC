4ec23a4bc5439f849e931d24135f07effdb404c6
pinchyCZN <mail2@crazynation.org>
2013-05-31 14:03:27 -0500

    update pragma warnings

--- pragma.h
+++ pragma.h
@@ -1,4 +1,4 @@
-#pragma warning(disable:4013)
-#pragma warning(disable:4047)
-#pragma warning(disable:4024)
+#pragma warning(disable:4013) //'function' undefined; assuming extern returning int
+#pragma warning(disable:4024) //'function' : different types for formal and actual parameter 'number'
+#pragma warning(disable:4047) //'operator' : 'identifier1' differs in levels of indirection from 'identifier2'
 #pragma warning(error:4189) //local variable init but not referenced
