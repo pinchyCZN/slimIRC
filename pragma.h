4343b10ab137f404daff733e8c9cc475c1d4e4b3
pinchyCZN <mail2@crazynation.org>
2013-06-14 19:18:37 -0500

    updated pragma.h to have some warnings as errors

--- pragma.h
+++ pragma.h
@@ -1,4 +1,8 @@
 #pragma warning(disable:4013) //'function' undefined; assuming extern returning int
 #pragma warning(disable:4024) //'function' : different types for formal and actual parameter 'number'
 #pragma warning(disable:4047) //'operator' : 'identifier1' differs in levels of indirection from 'identifier2'
-#pragma warning(error:4189) //local variable init but not referenced
+
+#pragma warning(error:4189) //local variable initialized but not referenced
+#pragma warning(error:4700) //local variable 'x' used without having been initialized
+#pragma warning(error:4020) //too many actual parameters
+#pragma warning(error:4101) //unreferenced local variable
