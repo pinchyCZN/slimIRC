9e8cfc3c5952f94444ab815d686d24889d08737c
pinchyCZN <pinchyCZN@gmail.com>
2015-04-06 08:44:19 -0500

    removed save nick

--- mdi_crap.c
+++ mdi_crap.c
@@ -1041,27 +1041,18 @@ int update_nick_in_list(HWND hlist,const char *oldnick,const char *newnick)
 	}
 	return index;
 }
-int save_new_nick(const char *nick)
-{
-	if(nick && nick[0]!=0){
-		write_ini_str("SETTINGS","NICK",nick);
-	}
-	return TRUE;
-}
+
 int update_nick(void *session,const char *oldnick,const char *nick)
 {
-	int i,save_nick=FALSE;
+	int i;
 	for(i=0;i<sizeof(irc_windows)/sizeof(IRC_WINDOW);i++){
 		if(irc_windows[i].session==session){
 			update_nick_in_list(irc_windows[i].hlist,oldnick,nick);
 			if(strcmp(irc_windows[i].nick,oldnick)==0){
 				strncpy(irc_windows[i].nick,nick,sizeof(irc_windows[i].nick));
-				save_nick=TRUE;
 			}
 		}
 	}
-	if(save_nick)
-		save_new_nick(nick);
 	return TRUE;
 }
 int update_user_nick(void *session,char *nick)
